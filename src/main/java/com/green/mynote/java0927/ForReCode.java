dd7197dcd6dd486b193d93c4b6724ef67342e7aa
Noeul <lovesatr42@gmail.com>
2023-10-04 13:18:38 +0900

    fisrt

--- src/main/java/com/green/mynote/java0927/ForReCode.java
+++ src/main/java/com/green/mynote/java0927/ForReCode.java
@@ -22,7 +22,7 @@ public class ForReCode {
             else{
                 System.out.print("");
             }
-            if( i % line == 0){
+            if( i % line == 8){
                 zen++;
                 wun = 1;
                 System.out.println();
