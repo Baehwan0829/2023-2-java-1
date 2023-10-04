466e13d6ef7bfb01a34c5334f7ee869ec8953b77
Noeul <lovesatr42@gmail.com>
2023-10-05 08:59:49 +0900

    10.4. 19:59

--- src/main/java/com/green/mynote/java0927/ForReCode.java
+++ src/main/java/com/green/mynote/java0927/ForReCode.java
@@ -6,6 +6,7 @@ public class ForReCode {
     public static void main(String[] args){
         /*
         이 짓은 하지말자
+        for을 1회 사용코드
          */
         Scanner s = new Scanner(System.in);
         System.out.print(" * 를 출력할 수를 입력하세요 : ");
@@ -14,7 +15,7 @@ public class ForReCode {
         System.out.println("-----------------------------");
         int wun = 1;
         int zen = 1;
-        // for을 1회 사용코드
+        //
         for(int i=1; i <= line * line; i++){
             if( wun++ <= zen){
                 System.out.print("*");
