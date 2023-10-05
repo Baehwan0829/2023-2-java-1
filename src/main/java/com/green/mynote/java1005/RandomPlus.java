bd58d16c241f7595130b22d6bcf4d9c66aa7901b
Noeul <lovesatr42@gmail.com>
2023-10-05 20:30:46 +0900

    10.5 . 20:30

--- src/main/java/com/green/mynote/java1005/RandomPlus.java
+++ src/main/java/com/green/mynote/java1005/RandomPlus.java
@@ -15,7 +15,25 @@ import java.util.Scanner;
 public class RandomPlus {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-
-        String plus = s.nextLine();
+        int enhance = 1;
+        System.out.printf("강화단계 : %d\n go / stop : \n",enhance);
+        String level = s.nextLine();
+        int min = 0, max = 22;
+        int rd = (int)(Math.random()*(max-1))+min;
+        //
+        Loop1 : while (0 <= enhance && enhance <= 22){
+            if (level) {
+                if (("go").equals(rd)) {
+                    System.out.printf("지금 강화단계: %d\n", (rd > 2) ? ++enhance : --enhance);
+                }
+                else if (0 <= enhance && enhance <= 22) {
+                    System.out.println("Go/Stop : ");
+                    level = s.nextLine();
+                }
+                else {
+                    break Loop1;
+                }
+            }
+        }
     }
 }
